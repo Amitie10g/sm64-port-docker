78763ea9f42c3f03f09fd34464fc52c11a21b7f2
Davod <davidkingnt@gmail.com>
2021-03-02 22:47:53 -0300

    Updates

--- Dockerfile
+++ Dockerfile
@@ -7,8 +7,8 @@ RUN	curl -L https://github.com/amitie10g/sm64-port/archive/master.zip --output s
 	ren sm64-port-master sm64-port && \
 	del sm64-port.zip
 
-COPY build.sh C:\\Users\\ContainerUser\\sm64-port\\build.sh
+COPY build.sh C:\\msys64\\home\\ContainerUser\\sm64-port\\build.sh
 
-WORKDIR C:\\Users\\ContainerUser\\sm64-port
+WORKDIR C:\\msys64\\home\\ContainerUser\\sm64-port\\
 ENV MSYSTEM=MINGW64
-CMD ["bash", "/home/sm64/sm64-port/build.sh"]
+CMD ["bash", "/home/ContainerUser/sm64-port/build.sh"]
--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
-#!/bin/env busybox sh
+#!/bin/env bash
 
-if ln -s /home/sm64/data/* . &> /dev/null ; then
+if ln -s /home/ContainerUser/data/baserom.??.z64 . &> /dev/null ; then
 	if make VERSION="${VERSION:-us}"; then
-		cp build/*_pc/sm64* /home/sm64/data/ &> /dev/null
+		cp ./build/*_pc/sm64* /home/ContainerUser/data/ &> /dev/null
 	fi
 	exit
 fi
 
 echo 'Usage:'
 echo ''
-echo 'docker run -v <path_to_data>:C:\msys64\home\sm64\data -e "VERSION=<version>" amitie10g/sm64-port'
+echo 'docker run -v <path_to_data>:C:\home\ContainerUser\data -e "VERSION=<version>" amitie10g/sm64-port'
 echo ''
 echo 'Add "bash" or "cmd" at the end for an interactive shell'
